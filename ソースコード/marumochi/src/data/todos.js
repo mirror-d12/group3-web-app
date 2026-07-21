b568eca71a452a6d22f5d0c6b4a4656dacaf2ae9
大槻　千聖 <2423227@mwu.jp>
2026-07-21 22:22:11 +0900

    ダミーユーザーデータとTODOデータを追加

---
+++ ソースコード/marumochi/src/data/todos.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+
+    userId: 1,
+
+    title: "サンプルTODO",
+
+    hasDeadline: true,
+
+    deadline: new Date(),
+
+    repeat: false,
+
+    repeatType: null,
+
+    progress: 0,
+
+    completed: false,
+
+    createdAt: new Date(),
+  },
+];
---
+++ ソースコード/marumochi/src/data/users.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: 1,
+
+    userName: "テストユーザー",
+
+    email: "test@test.com",
+
+    password: "1234",
+
+    friends: [],
+
+    friendCount: 0,
+
+    loginDays: 0,
+
+    totalTodoCount: 0,
+
+    createdAt: "2026-07-21",
+
+    isLogin: false,
+
+    notification: true,
+
+    profileImage: "profile1.png",
+
+    lastLogin: null,
+  },
+];
