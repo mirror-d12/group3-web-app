5a08c9ecb61b70aba9f39b85c0229d13b28246c6
大槻　千聖 <2423227@mwu.jp>
2026-07-22 21:58:25 +0900

    パスワード表示非表示のアイコンを追加

--- ソースコード/marumochi/src/components/common/PasswordField.vue
+++ ソースコード/marumochi/src/components/common/PasswordField.vue
@@ -2,17 +2,29 @@
   <v-text-field
     v-model="model"
     :label="label"
-    :append-inner-icon="show ? 'mdi-eye-off' : 'mdi-eye'"
     :type="show ? 'text' : 'password'"
     variant="outlined"
     bg-color="white"
     hide-details
-    @click:append-inner="show = !show"
-  />
+  >
+    <!-- 右側のアイコン部分をカスタム画像に置き換え -->
+    <template #append-inner>
+      <img
+        :src="show ? showIcon : hideIcon"
+        alt="パスワード表示切り替え"
+        class="eye-icon"
+        @click="show = !show"
+      />
+    </template>
+  </v-text-field>
 </template>
 
 <script setup>
 import { ref } from "vue";
+
+// src/images/ から画像をインポート
+import hideIcon from "@/images/password_hide.png";
+import showIcon from "@/images/password_show.png";
 
 const show = ref(false);
 const model = defineModel();
@@ -24,3 +36,12 @@ defineProps({
   },
 });
 </script>
+
+<style scoped>
+.eye-icon {
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  object-fit: contain;
+}
+</style>
