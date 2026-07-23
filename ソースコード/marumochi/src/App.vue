53abf0b852199b6d72bba791e2885126419dc266
大槻　千聖 <2423227@mwu.jp>
2026-07-23 15:47:10 +0900

    主要画面に追加するためのルーター設定

--- ソースコード/marumochi/src/App.vue
+++ ソースコード/marumochi/src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <v-app>
-    <router-view />
+    <v-main
+      :class="[
+        'app-main',
+        {
+          'app-main--with-footer': route.meta.showFooter,
+        },
+      ]"
+    >
+      <router-view />
+    </v-main>
+
+    <BottomNavigation v-if="route.meta.showFooter" />
   </v-app>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+
+import BottomNavigation from "./components/common/BottomNavigation.vue";
+
+const route = useRoute();
+</script>
 
 <style>
 html,
 body,
 #app {
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   margin: 0;
   padding: 0;
+}
+
+.app-main {
+  min-height: 100vh;
 }
 </style>
--- ソースコード/marumochi/src/router/index.js
+++ ソースコード/marumochi/src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/RegisterView.vue";
 import PasswordResetView from "../views/PasswordResetView.vue";
 import NewPasswordView from "../views/NewPasswordView.vue";
+
 import HomeView from "../views/HomeView.vue";
 import TodoView from "../views/TodoView.vue";
 import FriendView from "../views/FriendView.vue";
@@ -15,43 +16,86 @@ const routes = [
     path: "/",
     redirect: "/login",
   },
+
   {
     path: "/login",
+    name: "Login",
     component: LoginView,
+    meta: {
+      showFooter: false,
+    },
   },
+
   {
     path: "/register",
+    name: "Register",
     component: RegisterView,
+    meta: {
+      showFooter: false,
+    },
   },
+
   {
     path: "/password-reset",
     name: "PasswordReset",
     component: PasswordResetView,
+    meta: {
+      showFooter: false,
+    },
   },
+
   {
     path: "/new-password",
     name: "NewPassword",
     component: NewPasswordView,
+    meta: {
+      showFooter: false,
+    },
   },
+
   {
     path: "/home",
+    name: "Home",
     component: HomeView,
+    meta: {
+      showFooter: true,
+    },
   },
+
   {
     path: "/todo",
+    name: "Todo",
     component: TodoView,
+    meta: {
+      showFooter: true,
+    },
   },
+
   {
     path: "/friend",
+    name: "Friend",
     component: FriendView,
+    meta: {
+      showFooter: true,
+    },
   },
+
   {
     path: "/mypage",
+    name: "MyPage",
     component: MyPageView,
+    meta: {
+      showFooter: true,
+    },
   },
+
   {
     path: "/contact",
+    name: "Contact",
     component: ContactView,
+    meta: {
+      showFooter: true,
+    },
   },
 ];
 
