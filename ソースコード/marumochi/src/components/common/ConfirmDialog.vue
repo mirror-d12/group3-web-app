04bd0cff591fda2b8bcd83d6f15a3c7d4225afaa
大槻　千聖 <2423227@mwu.jp>
2026-07-27 21:34:51 +0900

    プロフィール画面のバグ修正

--- ソースコード/marumochi/src/components/common/ConfirmDialog.vue
+++ ソースコード/marumochi/src/components/common/ConfirmDialog.vue
@@ -1,49 +1,4 @@
 <template>
-  <v-dialog v-model="model" max-width="350">
-    <v-card>
-      <v-card-title>
-        {{ title }}
-      </v-card-title>
-
-      <v-card-text>
-        {{ message }}
-      </v-card-text>
-
-      <v-card-actions>
-        <v-spacer />
-
-        <v-btn text @click="cancel"> キャンセル </v-btn>
-
-        <v-btn color="primary" @click="ok"> OK </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-</template>
-
-<script setup>
-const model = defineModel();
-
-const emit = defineEmits(["ok", "cancel"]);
-
-defineProps({
-  title: String,
-
-  message: String,
-});
-
-function ok() {
-  emit("ok");
-
-  model.value = false;
-}
-
-function cancel() {
-  emit("cancel");
-
-  model.value = false;
-}
-</script>
-<template>
   <v-dialog v-model="model" max-width="560">
     <v-card class="confirm-dialog-card">
       <!-- タイトル -->
